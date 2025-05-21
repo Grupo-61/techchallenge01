e069da1eb2f68f6e9062939eb894d8bf4963f34f
ordepzero <ulissesph@gmail.com>
2025-05-21 00:29:13 -0300

    Ajuste configuração swagger

--- api/app.py
+++ api/app.py
@@ -3,12 +3,13 @@ from flasgger import Swagger
 import yaml
 import os
 
-app = create_app()
-
-with open("apidocs/swagger_config.yml", "r") as file:
+swagger_path = os.path.join(os.path.dirname(__file__), "apidocs", "swagger_config.yml")
+with open(swagger_path, "r") as file:
     swagger_config = yaml.safe_load(file)
 
 swagger_config["host"] = os.getenv("API_HOST", "localhost:5000") 
+
+app = create_app()
 
 swagger = Swagger(app, template=swagger_config)
 
